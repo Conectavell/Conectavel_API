efad20ced2908a2b941f764f1ec7e5c5ea851b54
Gabriel Ferreira Amorim <gabrielamorim15436@gmail.com>
2024-07-02 00:50:36 -0300

    tentativa 5

--- projetc/src/test/java/com/conectavel/projetc/ProjetcApplicationTests.java
+++ projetc/src/test/java/com/conectavel/projetc/ProjetcApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
-@SpringBootTest(classes = ApiApplication.class)
+@SpringBootTest
 class ProjetcApplicationTests {
 
 	@Test
